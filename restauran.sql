80e5f3716be4be2fe581921eb8f8f3d8e08140ff
Rey <thebestalpha2.3@gmail.com>
2022-04-29 00:23:09 -0600

    SQL de mesas

--- restauran.sql
+++ restauran.sql
@@ -1,6 +1,24 @@
 ﻿# Host: localhost  (Version 5.5.5-10.4.22-MariaDB)
-# Date: 2022-04-25 20:22:59
+# Date: 2022-04-29 00:22:57
 # Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Structure for table "mesas"
+#
+
+DROP TABLE IF EXISTS `mesas`;
+CREATE TABLE `mesas` (
+  `id_mesa` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `capacidad` varchar(255) DEFAULT '2',
+  `estado` varchar(1) DEFAULT '1' COMMENT '1=activo, 0=eliminado',
+  PRIMARY KEY (`id_mesa`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "mesas"
+#
 
 
 #
@@ -15,7 +33,7 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(255) DEFAULT '',
   `estado` varchar(1) DEFAULT '1' COMMENT '1=Activo, 1=Eliminado',
   PRIMARY KEY (`id_usuario`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 #
 # Data for table "usuarios"
